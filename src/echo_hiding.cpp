58fe7ed0c6eea48da9a5b6543c650d82c2b094ed
Matej Matuska <75834032+matejmatuska@users.noreply.github.com>
2023-03-03 22:34:37 +0100

    echo_hiding: Add mixer (smoothing)
    Use "mixer" to create smooth transitions between the two different
    echos. This significantly reduces "artifacts" and perceivibility of the
    transitions.
    
    The transition between the two different echos is linear, in the future
    a different function can be used.

--- src/echo_hiding.cpp
+++ src/echo_hiding.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <complex>
 #include <vector>
@@ -8,37 +9,77 @@
 #define ECHO_DELAY_ZERO 250
 #define ECHO_DELAY_ONE 300
 
-#define ECHO_AMP_ZERO 0.5
-#define ECHO_AMP_ONE 0.5
+#define ECHO_AMP_ZERO 0.6
+#define ECHO_AMP_ONE 0.6
 
 #define KERNEL_LEN 300
 
+#define USE_SMOOTHING 1
+#define SMOOTHING_PCT 0.95
 
 EchoHidingEmbedder::EchoHidingEmbedder(std::istream& data) :
     Embedder<double>::Embedder(data),
-    echo_zero(KERNEL_LEN, 0),
-    echo_one(KERNEL_LEN, 0),
-    delayed(in_frame.size() + KERNEL_LEN - 1, 0),
-    conv_zero(in_frame, echo_zero, delayed),
-    conv_one(in_frame, echo_one, delayed)
+    kernel_zero(KERNEL_LEN, 0),
+    kernel_one(KERNEL_LEN, 0),
+    echo_zero(in_frame.size() + KERNEL_LEN - 1, 0),
+    echo_one(in_frame.size() + KERNEL_LEN - 1, 0),
+    mixer(2 * in_frame.size(), bit),
+    conv_zero(in_frame, kernel_zero, echo_zero),
+    conv_one(in_frame, kernel_one, echo_one)
 {
-    echo_zero[0] = 1;
-    echo_one[0] = 1;
-    echo_zero[ECHO_DELAY_ZERO - 1] = ECHO_AMP_ZERO;
-    echo_one[ECHO_DELAY_ONE - 1] = ECHO_AMP_ONE;
+    kernel_zero[ECHO_DELAY_ZERO - 1] = ECHO_AMP_ZERO;
+    kernel_one[ECHO_DELAY_ONE - 1] = ECHO_AMP_ONE;
+}
+
+template<class ForwardIt>
+void linspace(ForwardIt first, ForwardIt last, double from, double to) {
+    const double step = (to - from) / (last - first);
+    double val = from;
+    for (; first != last; first++) {
+        *first = val;
+        val += step;
+    }
+}
+
+void EchoHidingEmbedder::update_mixer(char bit_from, char bit_to) {
+    const int start = SMOOTHING_PCT * in_frame.size();
+    const int end = mixer.size() - start;
+
+    if (bit_from == bit_to) {
+        // don't overwrite the part from last call
+        std::fill(mixer.begin() + start, mixer.end(), bit_from);
+        return;
+    }
+
+    // make the slope
+    linspace(mixer.begin() + start, mixer.begin() + end, bit_from, bit_to);
+    // TODO this fills a little bit more than needed
+    std::fill(mixer.begin() + end, mixer.end(), bit_to);
 }
 
 void EchoHidingEmbedder::embed() {
     // create echo
-    if (get_bit()) {
-        conv_one.exec();
+    conv_one.exec();
+    conv_zero.exec();
+
+    if (USE_SMOOTHING) {
+        update_mixer(bit, next_bit);
+
+        // add the echo to the signal
+        for (std::size_t i = 0; i < in_frame.size(); i++) {
+            out_frame[i] = in_frame[i] + echo_one[i] * mixer[i] +
+                           echo_zero[i] * (1 - mixer[i]);
+        }
+        // shift the mixer
+        std::move(mixer.begin() + (mixer.size() / 2), mixer.end(), mixer.begin());
     } else {
-        conv_zero.exec();
+        for (std::size_t i = 0; i < in_frame.size(); i++) {
+            double echo = bit ? echo_one[i] : echo_zero[i];
+            out_frame[i] = in_frame[i] + echo;
+        }
     }
-    // add the echo to the signal
-    for (std::size_t i = 0; i < in_frame.size(); i++) {
-        out_frame[i] = delayed[i]; // TODO maybe mixer
-    }
+    bit = next_bit;
+    next_bit = get_bit();
 }
 
 EchoHidingExtractor::EchoHidingExtractor() : Extractor<double>(),
--- src/echo_hiding.h
+++ src/echo_hiding.h
@@ -6,7 +6,6 @@
 #include "embedder.h"
 #include "extractor.h"
 #include "fft.h"
-#include "processing.h"
 
 
 class EchoHidingEmbedder : public Embedder<double> {
@@ -16,12 +15,16 @@ class EchoHidingEmbedder : public Embedder<double> {
         void embed() override;
 
     private:
+        void update_mixer(char bit_from, char bit_to);
+
+        char bit = get_bit();
+        char next_bit = get_bit();
+
+        std::vector<double> kernel_zero;
+        std::vector<double> kernel_one;
         std::vector<double> echo_zero;
         std::vector<double> echo_one;
-        std::vector<double> delayed;
-
-        char c = 0;
-        int bit_idx = 0;
+        std::vector<double> mixer;
 
         Conv conv_zero;
         Conv conv_one;
